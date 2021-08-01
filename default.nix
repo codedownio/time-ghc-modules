02033ddc3d71095df754a0d14cd7fa7f85f86568
Tom McLaughlin <tom@codedown.io>
2021-08-01 02:54:54 -0700

    Add nix derivation

---
+++ default.nix
@@ -0,0 +1,27 @@
+{
+  pkgs ? import (import ./pinned-nixpkgs.nix) {}
+}:
+
+let
+  script = ./time-ghc-modules;
+  process = ./scripts/process;
+  index = ./dist/index.html;
+
+in
+
+with pkgs;
+
+runCommand "time-ghc-modules" {
+  propagatedBuildInputs = [makeWrapper];
+  inherit script process index;
+} ''
+  mkdir -p $out/bin
+  cp $script $out/bin/time-ghc-modules
+  wrapProgram $out/bin/time-ghc-modules --prefix PATH : ${lib.makeBinPath [ sqlite ]}
+
+  mkdir -p $out/bin/scripts
+  ln -s $process $out/bin/scripts/process
+
+  mkdir -p $out/bin/dist
+  ln -s $index $out/bin/dist/index.html
+''
---
+++ pinned-nixpkgs.nix
@@ -0,0 +1,6 @@
+
+builtins.fetchGit {
+  url = "https://github.com/NixOS/nixpkgs.git";
+  rev = "973910f5c31b9ba6c171c33a8bd7199990b14c72";
+  ref = "release-21.05";
+}
